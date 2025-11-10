2f7889cfe316cb9497b320bcf73b787010b8f4db
Foundations-of-Frontend <azra.arnautovic@stu.ibu.edu.ba>
2025-11-10 22:56:52 +0100

    Added Swagger documentation, wrote annotations and finished testing, altered API routes, and added AuthRoute.php cuz i forgot about it last time

---
+++ backend/public/v1/docs/doc_setup.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @OA\Info(
+ *   title="API",
+ *   description="Bosnia Rentals API",
+ *   version="1.0",
+ *   @OA\Contact(
+ *     email="azra.arnautovic@atu.ibu.edu.ba",
+ *     name="Web Programming"
+ *   )
+ * ),
+ * @OA\Server(
+ *     url="http://localhost/AzraArnautovic/web-programming/backend",
+ *     description="API server"
+ * ),
+ * @OA\SecurityScheme(
+ *     securityScheme="ApiKey",
+ *     type="apiKey",
+ *     in="header",
+ *     name="Authentication"
+ * )
+ */
