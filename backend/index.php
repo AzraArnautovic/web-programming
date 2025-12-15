227a1ef6cbe8a04a4c94f5f41ccbd36ea071fbb5
Foundations-of-Frontend <azra.arnautovic@stu.ibu.edu.ba>
2025-12-15 15:09:53 +0100

    added CORS block

--- backend/index.php
+++ backend/index.php
@@ -1,4 +1,13 @@
 <?php
+header("Access-Control-Allow-Origin: https://shark-app-ioj5q.ondigitalocean.app");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Credentials: true");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 require 'vendor/autoload.php'; //run autoloader
 require_once __DIR__ . '/rest/services/UsersService.php';
 require_once __DIR__ . '/rest/services/ReservationsService.php';
