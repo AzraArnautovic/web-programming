3e0cc8cee557b1a27f7b50ef4ff8296cfba9411c
Foundations-of-Frontend <azra.arnautovic@stu.ibu.edu.ba>
2025-12-15 21:21:14 +0100

    fix

--- backend/public/v1/docs/swagger.php
+++ backend/public/v1/docs/swagger.php
@@ -3,14 +3,14 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 require __DIR__ . '/../../../vendor/autoload.php';
-
-define('LOCALSERVER', 'http://localhost/AzraArnautovic/web-programming/backend');
 define('PRODSERVER', 'https://starfish-app-q5czd.ondigitalocean.app');
+define('LOCALSERVER', 'http://localhost/AzraArnautovic/web-programming/backend');
+
 
 if($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1'){
-    define('BASE_URL', LOCALSERVER);
-} else {
     define('BASE_URL', PRODSERVER);
+} else {
+    define('BASE_URL', LOCALSERVER) ;
 }
 
 $openapi = \OpenApi\Generator::scan([
