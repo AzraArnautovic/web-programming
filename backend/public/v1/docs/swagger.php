47c434514c94b1d16f2a5d7efe033e548990f8a2
Foundations-of-Frontend <azra.arnautovic@stu.ibu.edu.ba>
2025-12-17 14:51:16 +0100

    Revert "fix"
    This reverts commit 3e0cc8cee557b1a27f7b50ef4ff8296cfba9411c.

--- backend/public/v1/docs/swagger.php
+++ backend/public/v1/docs/swagger.php
@@ -3,14 +3,14 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 require __DIR__ . '/../../../vendor/autoload.php';
-define('PRODSERVER', 'https://starfish-app-q5czd.ondigitalocean.app');
-define('LOCALSERVER', 'http://localhost/AzraArnautovic/web-programming/backend');
 
+define('LOCALSERVER', 'http://localhost/AzraArnautovic/web-programming/backend');
+define('PRODSERVER', 'https://starfish-app-q5czd.ondigitalocean.app');
 
 if($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1'){
-    define('BASE_URL', PRODSERVER);
+    define('BASE_URL', LOCALSERVER);
 } else {
-    define('BASE_URL', LOCALSERVER) ;
+    define('BASE_URL', PRODSERVER);
 }
 
 $openapi = \OpenApi\Generator::scan([
